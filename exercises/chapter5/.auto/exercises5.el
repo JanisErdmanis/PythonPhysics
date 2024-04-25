fa9c46244a88176a850d5a60d4c41e330eed9790
Jānis Erdmanis <akels14@gmail.com>
2024-04-25 23:04:44 +0300

    Delete exercises directory

--- exercises/chapter5/.auto/exercises5.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "exercises5"
- (lambda ()
-    (TeX-run-style-hooks
-     "latex2e"
-     "art12"
-     "article"
-     "12pt"
-     "setup")))
-
--- exercises/chapter6/.auto/exercises6.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "exercises6"
- (lambda ()
-    (TeX-run-style-hooks
-     "latex2e"
-     "art12"
-     "article"
-     "12pt"
-     "setup")))
-
